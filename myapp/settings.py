b6130841ce0b4e53ff59958bb3fd3307851756b5
AlexeyTozik <tozikalexey@gmail.com>
2023-09-23 16:08:01 +0300

    update: add bootstrap4 files

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mywebapp'
+    'mywebapp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
